2c4cbfd992a2c2b5557ce633344233498f082098
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2024-08-05 14:41:25 +0600

    start

---
+++ React01/src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import './App.css'
+
+
+const App = () => {
+  let [color, setColor]= useState("black")
+  return (
+    <div className='main'
+    style={{backgroundColor : color}}
+    >
+      <div className='btn-list'>
+        <button
+        onClick={()=>  setColor("red")}
+        style={{backgroundColor: "red"}}
+        >
+          red
+        </button>
+        <button
+        onClick={()=>{  setColor("blue")}}
+        style={{backgroundColor: "blue"}}
+        >
+          blue
+        </button>
+        <button
+        onClick={()=>{  setColor("yellow")}}
+        style={{backgroundColor: "yellow"}}
+        >
+          yellow
+        </button>
+        <button
+        onClick={()=>{  setColor("olive")}}
+        style={{backgroundColor: "olive"}}
+        >
+          olive
+        </button>
+        <button
+        onClick={()=>{  setColor("green")}}
+        style={{backgroundColor: "green"}}
+        >
+          green
+        </button>
+        <button
+        onClick={()=>{  setColor("lightblue")}}
+        style={{backgroundColor: "lightblue"}}
+        >
+          lightblue
+        </button>
+        <button
+        onClick={()=> setColor("gray")}
+        style={{backgroundColor: "gray"}}
+        >
+          gray
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
